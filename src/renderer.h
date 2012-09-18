7508bc65b062d33fedc5ceb01fb460d84cfbf1c3
Kyle Weicht <kyle@awesomekyle.com>
2012-09-17 22:21:56 -0700

    Adding separate renderer objects

---
+++ src/renderer.h
@@ -0,0 +1,20 @@
+/*! @file renderer.h
+ *  @brief TODO: Add the purpose of this module
+ *  @author Kyle Weicht
+ *  @date 9/17/12 10:15 PM
+ *  @copyright Copyright (c) 2012 Kyle Weicht. All rights reserved.
+ */
+#ifndef __renderer_renderer__
+#define __renderer_renderer__
+
+#ifdef __cplusplus
+extern "C" { // Use C linkage
+#endif 
+
+/* TODO: Add public declarations here */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* include guard */
---
+++ src/renderer_forward.h
@@ -0,0 +1,20 @@
+/*! @file renderer_forward.h
+ *  @brief TODO: Add the purpose of this module
+ *  @author Kyle Weicht
+ *  @date 9/17/12 10:19 PM
+ *  @copyright Copyright (c) 2012 Kyle Weicht. All rights reserved.
+ */
+#ifndef __renderer_forward_renderer_forward__
+#define __renderer_forward_renderer_forward__
+
+#ifdef __cplusplus
+extern "C" { // Use C linkage
+#endif 
+
+/* TODO: Add public declarations here */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* include guard */
