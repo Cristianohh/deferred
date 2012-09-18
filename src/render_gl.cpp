6689f57b4878ad8b05db86789b90480fa9b92566
Kyle Weicht <kyle@awesomekyle.com>
2012-09-17 21:03:02 -0700

    Adding header for geometry data

--- src/render_gl.cpp
+++ src/render_gl.cpp
@@ -4,12 +4,14 @@
  *  @copyright Copyright (c) 2012 Kyle Weicht. All rights reserved.
  */
 #include "render.h"
+
 #include <stddef.h>
 #include <stdio.h>
 #include "assert.h"
 #include "stb_image.h"
 #include "application.h"
 #include "vec_math.h"
+#include "geometry.h"
 
 #ifdef __APPLE__
     #include "TargetConditionals.h"
@@ -108,79 +110,6 @@ static const struct VertexDescription {
         { 0, 0 },
     }
 };
-
-static const VtxPosNormTex kCubeVertices[] =
-{
-    /* Top */
-    { {-0.5f,  0.5f, -0.5f}, { 0.0f,  1.0f,  0.0f}, {0.0f, 1.0f} },
-    { { 0.5f,  0.5f, -0.5f}, { 0.0f,  1.0f,  0.0f}, {1.0f, 1.0f} },
-    { { 0.5f,  0.5f,  0.5f}, { 0.0f,  1.0f,  0.0f}, {1.0f, 0.0f} },
-    { {-0.5f,  0.5f,  0.5f}, { 0.0f,  1.0f,  0.0f}, {0.0f, 0.0f} },
-
-    /* Bottom */
-    { {-0.5f, -0.5f, -0.5f}, { 0.0f, -1.0f,  0.0f}, {1.0f, 1.0f} },
-    { { 0.5f, -0.5f, -0.5f}, { 0.0f, -1.0f,  0.0f}, {0.0f, 1.0f} },
-    { { 0.5f, -0.5f,  0.5f}, { 0.0f, -1.0f,  0.0f}, {0.0f, 0.0f} },
-    { {-0.5f, -0.5f,  0.5f}, { 0.0f, -1.0f,  0.0f}, {1.0f, 0.0f} },
-
-    /* Left */
-    { {-0.5f, -0.5f,  0.5f}, {-1.0f,  0.0f,  0.0f}, {0.0f, 1.0f} },
-    { {-0.5f, -0.5f, -0.5f}, {-1.0f,  0.0f,  0.0f}, {1.0f, 1.0f} },
-    { {-0.5f,  0.5f, -0.5f}, {-1.0f,  0.0f,  0.0f}, {1.0f, 0.0f} },
-    { {-0.5f,  0.5f,  0.5f}, {-1.0f,  0.0f,  0.0f}, {0.0f, 0.0f} },
-
-    /* Right */
-    { { 0.5f, -0.5f,  0.5f}, { 1.0f,  0.0f,  0.0f}, {1.0f, 1.0f} },
-    { { 0.5f, -0.5f, -0.5f}, { 1.0f,  0.0f,  0.0f}, {0.0f, 1.0f} },
-    { { 0.5f,  0.5f, -0.5f}, { 1.0f,  0.0f,  0.0f}, {0.0f, 0.0f} },
-    { { 0.5f,  0.5f,  0.5f}, { 1.0f,  0.0f,  0.0f}, {1.0f, 0.0f} },
-
-    /* Front */
-    { {-0.5f, -0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f}, {0.0f, 1.0f} },
-    { { 0.5f, -0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f}, {1.0f, 1.0f} },
-    { { 0.5f,  0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f}, {1.0f, 0.0f} },
-    { {-0.5f,  0.5f, -0.5f}, { 0.0f,  0.0f, -1.0f}, {0.0f, 0.0f} },
-
-    /* Back */
-    { {-0.5f, -0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f}, {1.0f, 1.0f} },
-    { { 0.5f, -0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f}, {0.0f, 1.0f} },
-    { { 0.5f,  0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f}, {0.0f, 0.0f} },
-    { {-0.5f,  0.5f,  0.5f}, { 0.0f,  0.0f,  1.0f}, {1.0f, 0.0f} },
-};
-static const unsigned short kCubeIndices[] =
-{
-    3,1,0,
-    2,1,3,
-
-    6,4,5,
-    7,4,6,
-
-    11,9,8,
-    10,9,11,
-
-    14,12,13,
-    15,12,14,
-
-    19,17,16,
-    18,17,19,
-
-    22,20,21,
-    23,20,22
-};
-
-static const VtxPosTex kQuadVertices[] =
-{
-    { {-0.5f, -0.5f, 0.0f}, {0.0f, 1.0f} },
-    { { 0.5f, -0.5f, 0.0f}, {1.0f, 1.0f} },
-    { { 0.5f,  0.5f, 0.0f}, {1.0f, 0.0f} },
-    { {-0.5f,  0.5f, 0.0f}, {0.0f, 0.0f} },
-};
-static const unsigned short kQuadIndices[] =
-{
-    3,1,0,
-    2,1,3,
-};
-
 
 GLuint _compile_shader(GLenum shader_type, const char* filename) {
     MessageBoxResult result = kMBOK;
