a8de55a49e329851b3e89819aa1b3e964fe0af70
Kyle Weicht <kyle@awesomekyle.com>
2012-09-20 21:27:29 -0700

    16-bit color buffer

--- src/render_gl.cpp
+++ src/render_gl.cpp
@@ -508,7 +508,7 @@ void _resize_framebuffer(void) {
     CheckGLError();
 
     glBindTexture(GL_TEXTURE_2D, _color_texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, _width, _height, 0, GL_RGBA, GL_HALF_FLOAT, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _color_texture, 0);
